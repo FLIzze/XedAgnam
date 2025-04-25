5e3b317f39a6a0d60ba515fc9587200900853c25
HugoANDRIAMAMPIANINA <hugo.andria15@gmail.com>
2025-04-25 10:33:08 +0200

    init tanstack query

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,25 +1,38 @@
+import { queryClient } from "@/queries";
 import theme from "@/theme";
 import { ThemeProvider } from "@shopify/restyle";
+import { PersistQueryClientProvider } from "@tanstack/react-query-persist-client";
+import { createAsyncStoragePersister } from "@tanstack/query-async-storage-persister";
 import { Stack } from "expo-router";
+import AsyncStorage from "@react-native-async-storage/async-storage";
+
+const asyncStoragePersister = createAsyncStoragePersister({
+  storage: AsyncStorage,
+});
 
 export default function RootLayout() {
   return (
-    <ThemeProvider theme={theme}>
-      <Stack
-        screenOptions={{
-          contentStyle: { backgroundColor: theme.colors.background },
-        }}
-      >
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen
-          name="onboarding/index"
-          options={{ headerShown: false }}
-        />
-        {/* <Stack.Screen
+    <PersistQueryClientProvider
+      client={queryClient}
+      persistOptions={{ persister: asyncStoragePersister }}
+    >
+      <ThemeProvider theme={theme}>
+        <Stack
+          screenOptions={{
+            contentStyle: { backgroundColor: theme.colors.background },
+          }}
+        >
+          <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+          <Stack.Screen
+            name="onboarding/index"
+            options={{ headerShown: false }}
+          />
+          {/* <Stack.Screen
             name="manga/[mangaId]/index"
             options={}
           /> */}
-      </Stack>
-    </ThemeProvider>
+        </Stack>
+      </ThemeProvider>
+    </PersistQueryClientProvider>
   );
 }
