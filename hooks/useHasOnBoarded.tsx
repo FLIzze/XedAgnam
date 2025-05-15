76d088f0f35057ef9edffc5e4d5fd8ca14bd6a66
flizze <alexandre.bel@ynov.com>
2025-05-15 16:01:36 +0200

    merged

--- hooks/useHasOnBoarded.tsx
+++ hooks/useHasOnBoarded.tsx
@@ -1,8 +1,9 @@
 import AsyncStorage from "@react-native-async-storage/async-storage";
-import { router, useRootNavigationState } from "expo-router";
+import { router } from "expo-router";
 import { useEffect, useState } from "react";
 
 export function useHasOnBoarded() {
+<<<<<<< HEAD
     const [hasOnBoarded, setHasOnBoarded] = useState<boolean | null>(null);
     const [checkedStorage, setCheckedStorage] = useState(false);
     // const rootNavigationState = useRootNavigationState();
@@ -21,4 +22,23 @@ export function useHasOnBoarded() {
     // }, [rootNavigationState?.key, checkedStorage]);
 
     return { hasOnBoarded, checkedStorage };
+=======
+  const [hasOnBoarded, setHasOnBoarded] = useState<boolean | null>(null);
+  const [checkedStorage, setCheckedStorage] = useState(false);
+
+  useEffect(() => {
+    if (!checkedStorage) {
+      AsyncStorage.getItem("hasOnBoarded").then((value) => {
+        const onboarded = value === "true";
+        setHasOnBoarded(onboarded);
+        setCheckedStorage(true);
+        if (!onboarded) {
+          router.replace("/onboarding");
+        }
+      });
+    }
+  }, [checkedStorage]);
+
+  return { hasOnBoarded, checkedStorage };
+>>>>>>> 46ea4c88475b231a2102621b5593732a5987b91a
 }
